d7be285be392071eafa653f555b6a15490244de2
nicholasbay <bay.yong.wei@gmail.com>
2025-03-27 10:22:10 +0800

    Add queryFacilityNames(), deleteBooking(), rateFacility(), and queryRating() methods

--- Client/include/Client.hpp
+++ Client/include/Client.hpp
@@ -24,19 +24,20 @@ public:
     Client(const std::string &serverIp, int serverPort);
     ~Client();
 
+    std::string queryFacilityNames();
     std::string queryAvailability(std::string facilityName);
     std::string bookFacility(std::string facilityName, std::string dayOfWeek, std::string startTime, std::string endTime);
     std::string queryBooking(std::string bookingID);
     std::string changeBooking(std::string bookingID, std::string newDayOfWeek, std::string newStartTime, std::string newEndTime);
+    std::string deleteBooking(std::string bookingID);
     std::string monitorAvailability(std::string facilityName);
-    // TODO: idempotentOperation();
-    // TODO: nonIdempotentOperation();
-    std::string sendCustomMessage(std::string messageData);
+    std::string rateFacility(std::string facilityName, float rating);       // Non-idempotent operation
+    std::string queryRating(std::string facilityName);                      // Idempotent operation
+    std::string echoMessage(std::string messageData);
 
 private:
     void makeLocalSocketAddress(struct sockaddr_in *sa);
     void makeRemoteSocketAddress(struct sockaddr_in *sa, char *hostname, int port);
-    void printBoundSocketInfo();
     void sendRequest(const RequestMessage &request);
     std::string receiveResponse();
 };
