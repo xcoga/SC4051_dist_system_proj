765550fdfe6c5e2d23424b6fdeda1c347f836c69
nicholasbay <bay.yong.wei@gmail.com>
2025-03-27 10:23:08 +0800

    Add parsing of echo and query facility names responses

--- Client/src/ResponseParser.cpp
+++ Client/src/ResponseParser.cpp
@@ -3,8 +3,42 @@
 #include "Serializer.hpp"
 
 #include <iostream>
+#include <sstream>
 
-std::vector<std::string> ResponseParser::parseEchoResponse(const std::string &response)
+/* Public methods */
+std::vector<std::string> ResponseParser::parseQueryFacilityNamesResponse(const std::string &response)
+{
+    std::vector<std::string> parsedResponse;
+    std::string errorMessage;
+
+    if (isErrorResponse(response, errorMessage))
+    {
+        parsedResponse.push_back("Error");
+        parsedResponse.push_back(errorMessage);
+    }
+    else
+    {
+        parsedResponse.push_back("Facility Names");
+
+        std::istringstream responseStream(response);
+        std::string line;
+
+        if (std::getline(responseStream, line) && line == "status: SUCCESS")
+        {
+            while (std::getline(responseStream, line, ','))
+            {
+                if (!line.empty())
+                {
+                    parsedResponse.push_back(line);
+                }
+            }
+        }
+    }
+
+    return parsedResponse;
+}
+
+std::vector<std::string> ResponseParser::parseEchoMessageResponse(const std::string &response)
 {
     std::vector<std::string> parsedResponse;
 
@@ -14,4 +48,22 @@ std::vector<std::string> ResponseParser::parseEchoResponse(const std::string &re
     };
 
     return parsedResponse;
+}
+
+/* Private methods */
+bool ResponseParser::isErrorResponse(const std::string &response, std::string &errorMessage)
+{
+    std::istringstream responseStream(response);
+    std::string line;
+
+    if (std::getline(responseStream, line) && line.find("ERROR") == 0)
+    {
+        if (std::getline(responseStream, line) && line.find("message: ") == 0)
+        {
+            // Remove "message: " prefix
+            errorMessage = line.substr(9);
+        }
+        return true;
+    }
+    return false;
 }
